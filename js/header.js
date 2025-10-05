3ee3af0694e82c6e86dc77b0d22d184dfb1c9363
Richard <richardruizs1998@gmail.com>
2025-10-05 01:45:41 -0600

    Mejora visual header

--- js/header.js
+++ js/header.js
@@ -1,11 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   fetch("header.html")
-    .then(response => {
+    .then((response) => {
       if (!response.ok) throw new Error("Error al cargar el header");
       return response.text();
     })
-    .then(data => {
+    .then((data) => {
       document.body.insertAdjacentHTML("afterbegin", data);
+
+      const header = document.querySelector(".main-header");
+      const main = document.querySelector("main");
+
+      if (header && main) {
+        function ajustarMain() {
+          main.style.paddingTop = header.offsetHeight + "px";
+        }
+
+        ajustarMain();
+        window.addEventListener("resize", ajustarMain);
+
+        window.addEventListener("scroll", ajustarMain);
+      }
     })
-    .catch(error => console.error(error));
+    .catch((error) => console.error(error));
 });
